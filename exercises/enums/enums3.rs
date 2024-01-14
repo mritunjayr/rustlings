95b9153815581524e70b15692984024b8317ab8f
mritunjay <mritunjaykumar694@gmail.com>
2024-01-14 21:02:27 +0530

    quiz2 completed

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,70 +5,30 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+#[derive(Debug)]
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    // TODO: define the different variants used below
+    Move { x: u32, y: u32 },
+    Echo(String),
+    ChangeColor(u32, u32, u32),
+    Quit,
 }
 
-struct Point {
-    x: u8,
-    y: u8,
-}
-
-struct State {
-    color: (u8, u8, u8),
-    position: Point,
-    quit: bool,
-    message: String,
-}
-
-impl State {
-    fn change_color(&mut self, color: (u8, u8, u8)) {
-        self.color = color;
-    }
-
-    fn quit(&mut self) {
-        self.quit = true;
-    }
-
-    fn echo(&mut self, s: String) {
-        self.message = s
-    }
-
-    fn move_position(&mut self, p: Point) {
-        self.position = p;
-    }
-
-    fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message
-        // variants
-        // Remember: When passing a tuple as a function argument, you'll need
-        // extra parentheses: fn function((t, u, p, l, e))
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
 
-    #[test]
-    fn test_match_message_call() {
-        let mut state = State {
-            quit: false,
-            position: Point { x: 0, y: 0 },
-            color: (0, 0, 0),
-            message: "hello world".to_string(),
-        };
-        state.process(Message::ChangeColor(255, 0, 255));
-        state.process(Message::Echo(String::from("Hello world!")));
-        state.process(Message::Move(Point { x: 10, y: 15 }));
-        state.process(Message::Quit);
-
-        assert_eq!(state.color, (255, 0, 255));
-        assert_eq!(state.position.x, 10);
-        assert_eq!(state.position.y, 15);
-        assert_eq!(state.quit, true);
-        assert_eq!(state.message, "Hello world!");
+    for message in &messages {
+        message.call();
     }
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +30,18 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Append(times) => output.push(format!("{string}{}", "bar".repeat(*times))),
+
+                Command::Trim => output.push((&string.trim()).to_string()),
+
+                Command::Uppercase => output.push(String::from(&string.to_uppercase())),
+            }
         }
         output
     }
@@ -45,8 +50,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
